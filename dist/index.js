66d20c640768deb515e540ea86b87098cf9f33da
Ricardo Gama <ricardo@seegno.com>
2016-06-22 16:08:38 +0100

    Release 1.0.0

---
+++ dist/index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _jsonMask = require('json-mask');
+
+var _jsonMask2 = _interopRequireDefault(_jsonMask);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/**
+ * Export `bookshelf-mask` plugin.
+ */
+
+exports.default = function (Bookshelf) {
+  Bookshelf.Model = Bookshelf.Model.extend({
+    mask: function mask(scope, options) {
+      return (0, _jsonMask2.default)(this.toJSON(options), this.masks && this.masks[scope] || scope);
+    }
+  });
+
+  Bookshelf.Collection = Bookshelf.Collection.extend({
+    mask: function mask(scope, options) {
+      scope = this.model.prototype.masks && this.model.prototype.masks[scope] || scope;
+
+      return this.toJSON(options).map(function (model) {
+        return (0, _jsonMask2.default)(model, scope);
+      });
+    }
+  });
+};
+/**
+ * Module dependencies.
+ */
